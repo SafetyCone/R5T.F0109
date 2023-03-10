8f8e7b31a2743ad31c8ec39344340ba5c612a411
davidcoats <david@getsafetycone.com>
2023-03-09 23:17:16 -0800

    Initial commit.

---
+++ source/R5T.F0109/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.F0109
+{
+	/// <summary>
+	/// Assembly functionality survey functionality.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.F0109/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.F0109
+Assembly functionality survey functionality.
