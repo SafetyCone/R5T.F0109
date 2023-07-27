f52f918096b476dc2a3cc7f14f0d61667d54150e
davidcoats <david@getsafetycone.com>
2023-07-27 01:30:52 -0700

    Interim changes.

--- source/R5T.F0109/Code/Functionality/IAssemblyInstancesDescriptorOperator.cs
+++ source/R5T.F0109/Code/Functionality/IAssemblyInstancesDescriptorOperator.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Linq;
 
+using R5T.F0124;
 using R5T.T0132;
+using R5T.T0172;
 
 
 namespace R5T.F0109
@@ -8,6 +11,19 @@ namespace R5T.F0109
     [FunctionalityMarker]
     public partial interface IAssemblyInstancesDescriptorOperator : IFunctionalityMarker
     {
+        public IProjectFilePath[] Get_ProjectFilePaths(
+            AssemblyInstancesDescriptor instances,
+            bool orderAlphabetically = IValues.Default_Alphabetization_Constant)
+        {
+            var output = instances.Instances
+                .Select(x => x.ProjectFilePath)
+                .Append(instances.InstanceTypes
+                    .Select(x => x.ProjectFilePath))
+                .Distinct()
+                .OrderAlphabetically_If(x => x.Value, orderAlphabetically)
+                .Now();
 
+            return output;
+        }
     }
 }
